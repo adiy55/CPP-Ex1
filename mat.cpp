f841eb5b1d9c955fb3ad951e14688fc50736d421
adiy55 <adiyafe55@gmail.com>
2022-03-07 17:21:40 +0200

    Return empty string in mat function

--- mat.cpp
+++ mat.cpp
@@ -1,10 +1,5 @@
 #include "mat.hpp"
 
-using namespace std;
-
-string ariel::mat(int cols, int rows, char symb1, char symb2) {
-    if (cols % 2 == 0 || rows % 2 == 0) {
-        throw "Mat size is always odd";
-    }
-    return "1";
+std::string ariel::mat(int cols, int rows, char symb1, char symb2) {
+    return "";
 }
