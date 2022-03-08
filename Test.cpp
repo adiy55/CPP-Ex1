cc519c33f8af5a8b5a4c7fb8f99d5ba8332e9c62
adiy55 <adiyafe55@gmail.com>
2022-03-08 15:23:04 +0200

    Update test cases

--- Test.cpp
+++ Test.cpp
@@ -22,6 +22,7 @@ string nospaces(string input) {
 
 
 TEST_CASE ("Good input") {
+
             CHECK(nospaces(mat(9, 7, '@', '-')) == nospaces("@@@@@@@@@\n"
                                                             "@-------@\n"
                                                             "@-@@@@@-@\n"
@@ -36,23 +37,6 @@ TEST_CASE ("Good input") {
                                                              "@-----------@\n"
                                                              "@@@@@@@@@@@@@"));
 
-    // given the same symbol
-            CHECK(nospaces(mat(9, 7, '@', '@')) == nospaces("@@@@@@@@@\n"
-                                                            "@@@@@@@@@\n"
-                                                            "@@@@@@@@@\n"
-                                                            "@@@@@@@@@\n"
-                                                            "@@@@@@@@@\n"
-                                                            "@@@@@@@@@\n"
-                                                            "@@@@@@@@@"));
-
-    // single char matrix, should take the first symbol
-            CHECK(mat(1, 1, '#', '&') == "#");
-
-    // square matrix
-            CHECK(nospaces(mat(3, 3, '#', '!')) == nospaces("###\n"
-                                                            "#!#\n"
-                                                            "###"));
-
             CHECK(nospaces(mat(3, 5, '*', 'A')) == nospaces("***\n"
                                                             "*A*\n"
                                                             "*A*\n"
@@ -63,32 +47,87 @@ TEST_CASE ("Good input") {
                                                             "*AAA*\n"
                                                             "*****"));
 
-    // case-sensitive: changed symbol from uppercase to lowercase
+}
+
+TEST_CASE ("Given the same symbol") {
+
+            CHECK(nospaces(mat(9, 7, '@', '@')) == nospaces("@@@@@@@@@\n"
+                                                            "@@@@@@@@@\n"
+                                                            "@@@@@@@@@\n"
+                                                            "@@@@@@@@@\n"
+                                                            "@@@@@@@@@\n"
+                                                            "@@@@@@@@@\n"
+                                                            "@@@@@@@@@"));
+
+}
+
+TEST_CASE ("Single row or column matrix- should use the first symbol") {
+
+            CHECK(mat(1, 1, '#', '&') == "#");
+
+            CHECK(nospaces(mat(1, 9, '$', '@')) == nospaces("$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$\n"
+                                                            "$"));
+
+            CHECK(mat(9, 1, '*', '&') == "*********");
+
+}
+
+TEST_CASE ("Square matrix") {
+
+            CHECK(nospaces(mat(3, 3, '#', '!')) == nospaces("###\n"
+                                                            "#!#\n"
+                                                            "###"));
+
+}
+
+TEST_CASE ("Symbol and case-sensitivity") {
+
+    // changed symbol from uppercase to lowercase
             CHECK(nospaces(mat(5, 3, '*', 'A')) != nospaces(mat(5, 3, '*', 'a')));
 
-    // same size, using single (but different) chars
+    // same size matrix, using single (but different) chars
             CHECK(nospaces(mat(5, 5, '$', '$')) != nospaces(mat(5, 5, '*', '*')));
 
 }
 
 TEST_CASE ("Bad input") {
-    // even matrix size
+    // contains even numbers
             CHECK_THROWS(mat(10, 5, '$', '%'));
             CHECK_THROWS(mat(7, 20, '$', '%'));
             CHECK_THROWS(mat(0, 0, '$', '%'));
-    // negative matrix size
+
+}
+
+TEST_CASE ("Negative input size") {
+
             CHECK_THROWS(mat(-5, 0, '$', '%'));
-            CHECK_THROWS(mat(0, -9, '$', '%'));
+            CHECK_THROWS(mat(9, -9, '$', '%'));
             CHECK_THROWS(mat(-5, -1, '$', '%'));
-    // newline, tab, null terminator
+}
+
+TEST_CASE ("Invalid characters- newline, tab, null terminator, space") {
+
             CHECK_THROWS(mat(3, 5, '\n', '*'));
+
             CHECK_THROWS(mat(9, 7, '^', '\r'));
+
             CHECK_THROWS(mat(1, 3, '\n', '\t'));
+
             CHECK_THROWS(mat(1, 3, '\0', '&'));
+
             CHECK_THROWS(mat(3, 19, '5', '\0'));
-    // space character
+
             CHECK_THROWS(mat(5, 7, ' ', '7'));
+
             CHECK_THROWS(mat(7, 7, '@', ' '));
+
             CHECK_THROWS(mat(5, 5, ' ', ' '));
 
 }
