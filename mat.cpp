5c1334d8892efb6d12f9bd641ac0126d6ea846f1
adiy55 <adiyafe55@gmail.com>
2022-03-12 18:49:11 +0200

    Add documentation

--- mat.cpp
+++ mat.cpp
@@ -7,9 +7,9 @@ using std::invalid_argument;
 
 namespace ariel {
     /**
-     * Checks if input is valid. Throws exceptions accordingly.
+     * Throws exception if the input is invalid.
      */
-    void checkInput(int cols, int rows, char symb1, char symb2) {
+    void checkInput(int rows, int cols, char symb1, char symb2) {
         if (rows < 1 || cols < 1) {
             throw invalid_argument("Dimensions should be positive numbers!");
         }
@@ -28,7 +28,7 @@ namespace ariel {
      * Converts vector matrix to string. Adds a newline to every row except the last.
      * @return rug pattern as string
      */
-    string vectorToString(vector <vector<char>> matrix, int rows, int cols) {
+    string vectorToString(vector<vector<char>> matrix, int rows, int cols) {
         string res;
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
@@ -42,23 +42,23 @@ namespace ariel {
     }
 
     string mat(int cols, int rows, char symb1, char symb2) {
-        checkInput(cols, rows, symb1, symb2);
-        vector <vector<char>> matrix(rows, vector<char>(cols));
+        checkInput(rows, cols, symb1, symb2);
+        vector<vector<char>> matrix(rows, vector<char>(cols));
         int curr_row = 0;
         int curr_col = 0;
-        char curr_symbol = symb1;
+        char curr_symbol = symb1; // outer ring is always first symbol
         while (curr_row < rows && curr_col < cols) {
-            for (int i = curr_row; i < rows - curr_row; ++i) {
+            for (int i = curr_row; i < rows - curr_row; ++i) { // fill columns in current layer
                 matrix[i][curr_col] = curr_symbol;
                 matrix[i][cols - curr_col - 1] = curr_symbol;
             }
-            for (int i = curr_col; i < cols - curr_col; ++i) {
+            for (int i = curr_col; i < cols - curr_col; ++i) { // fill rows in current layer
                 matrix[curr_row][i] = curr_symbol;
                 matrix[rows - curr_row - 1][i] = curr_symbol;
             }
             curr_row += 1;
             curr_col += 1;
-            curr_symbol = curr_symbol == symb1 ? symb2 : symb1;
+            curr_symbol = curr_symbol == symb1 ? symb2 : symb1; // alternate symbols
         }
         return vectorToString(matrix, rows, cols); // vector is passed by value
     }
